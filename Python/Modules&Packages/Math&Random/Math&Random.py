5aef0e75020732385700d320cb57bae57d0e94bb
plai1 <plai1@dons.usfca.edu>
2020-09-24 04:02:17 -0700

    September 24

---
+++ Python/Modules&Packages/Math&Random/Math&Random.py
@@ -0,0 +1,46 @@
+import math
+from math import pi
+import random
+
+
+#Rounding Numbers
+value = 10.97685
+print(math.floor(value))
+print(math.ceil(value))
+print(round(value))
+
+#Mathematical Constants
+print(pi)
+print(math.e)
+print(math.tau)
+print(math.inf)
+print(math.nan)
+
+#Logarithmic Values
+math.e
+# Log Base e
+math.log(math.e)
+math.log(10)
+math.e ** 2.302585092994046
+
+
+#Trigonometrics Functions
+
+# Radians
+math.sin(10)
+math.degrees(pi/2)
+math.radians(180)
+
+
+
+
+#Random Module
+random.randint(0,100)
+random.randint(0,100)
+
+random.seed(101)
+random.randint(0,100)
+
+#Random with Sequences
+mylist = list(range(0,20))
+print(random.choice(mylist))
